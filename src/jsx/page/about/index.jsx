706f0ab4d700a13ff38af53fa67be2341b5fe7ca
Shaiz <k.shaiz@gmail.com>
2022-02-11 15:54:01 -0500

    Updated resume and about text

--- src/jsx/page/about/index.jsx
+++ src/jsx/page/about/index.jsx
@@ -65,12 +65,12 @@ const About = () => {
                 </Grid>
                 <Grid item xs={12}>
                   <Typography variant="h3">
-                    a Product Designer currently based in Toronto, Canada
+                    a Product Designer based in Toronto, Canada
                   </Typography>
                 </Grid>
                 <Grid item xs={12}>
                   <Typography variant="h5" style={{ marginTop: "1em" }}>
-                  Originally from India, I started my career as a UX Designer in Sydney (Australia), translating business goals into products that satisfy user needs. Over the course of more than 10 years, I have worked in multi-disciplinary teams, from small startups to multi-national corporations, launching products in different sectors such as FinTech, Digital Ads and eLearning. An avid learner, I am always seeking out opportunities to learn &amp; contribute towards user-centered product development.
+                  I started my career as a UX Designer in Sydney (Australia), translating business goals into products that satisfy user needs. Over the course of more than 10 years, I have worked in multi-disciplinary teams, from small startups to multinational corporations, launching products in different sectors such as FinTech, Digital Ads and eLearning. I recently moved to Canada (on PR) and am seeking out opportunities to learn &amp; contribute towards user-centered product development.
                   </Typography>
                 </Grid>
               </Grid>
